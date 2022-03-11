9ef56500755476eafbd8a7a08e5d2089ae16f26d
池兵兵 <297193634@qq.com>
2022-03-11 11:09:59 +0800

    test-freemarker

---
+++ spring-freemarker/src/main/java/com/example/springfreemarker/SpringFreemarkerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springfreemarker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFreemarkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFreemarkerApplication.class, args);
+    }
+
+}
