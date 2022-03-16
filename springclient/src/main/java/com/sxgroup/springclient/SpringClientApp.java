209411aec3afdcc56eb661467d596d594c16eafe
池兵兵 <297193634@qq.com>
2022-03-16 19:20:32 +0800

    集群测试类

--- springclient/src/main/java/com/sxgroup/springclient/SpringClientApp.java
+++ springclient/src/main/java/com/sxgroup/springclient/SpringClientApp.java
@@ -2,8 +2,10 @@ package com.sxgroup.springclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SpringClientApp {
     public static void main(String[] args) {
         SpringApplication.run(SpringClientApp.class, args);
--- springclient/src/main/java/com/sxgroup/springclient/controller/ConsumerController.java
+++ springclient/src/main/java/com/sxgroup/springclient/controller/ConsumerController.java
@@ -2,6 +2,8 @@ package com.sxgroup.springclient.controller;
 
 import com.sxgroup.springclient.bean.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -19,6 +21,28 @@ public class ConsumerController {
         String url = "http://127.0.0.1:8081/all";
         return this.restTemplate.getForObject(url, List.class);
     }
+
+    @Autowired
+    private DiscoveryClient client;
+
+    // Eureka客户端，可以获取到服务实例 信息
+    @RequestMapping("/test1")
+    public List<User> consumerTest1() {
+        List<String> list = client.getServices();
+        System.out.println("*服务列表*" + list);
+        List<ServiceInstance> srvList = client.getInstances("springcloud-demo-service");
+        for (ServiceInstance element : srvList) {
+
+        //●注册中心： ●访问测试： 2.4.注册微服务信息完善
+            System.out.println(element.getServiceId() + "\t" + element.getHost() + "\t" + element.getPort() + "\t" + element.getUri());
+        }
+        // 因为只有一个UserService,因此我们直接get(0)获取
+        ServiceInstance instance = srvList.get(0); // 获取ip和端口信息
+        String baseUrl = "http://" + instance.getHost() + ":" + instance.getPort() + "/all";
+        System.out.println("访问地址:" + baseUrl);
+        return this.restTemplate.getForObject(baseUrl, List.class);
+    }
+
 
 
 }
